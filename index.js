7fc6390e4666b7c703d90b43161044e9eeb0b3a3
Apoorva Ramakrishnan <apoorvar@comcast.net>
2021-06-12 15:41:17 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts); 
+
+function writeCards(arr, event){
+    const newArr = []; 
+    for(let i = 0; i < arr.length; i ++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArr; 
+}
+
+function countDown(number){
+    while(number > -1){
+        console.log(number); 
+        number--; 
+    }
+}
